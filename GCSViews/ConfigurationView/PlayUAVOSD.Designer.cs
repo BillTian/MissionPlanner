dab036e88dc49e58c91c65abcbfba2a1c7b87889
rentt <tt.ren.cn@gmail.com>
2015-03-17 10:56:18 +0800

    add playuavosd

--- GCSViews/ConfigurationView/PlayUAVOSD.Designer.cs
+++ GCSViews/ConfigurationView/PlayUAVOSD.Designer.cs
@@ -29,20 +29,32 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(357, 76);
+            this.button1.Location = new System.Drawing.Point(17, 423);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(246, 63);
+            this.button1.Size = new System.Drawing.Size(77, 30);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "保存参数";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(128, 423);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(77, 30);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "读取参数";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // PlayUAVOSD
             // 
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "PlayUAVOSD";
             this.Size = new System.Drawing.Size(673, 472);
@@ -53,6 +65,7 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
 
  
     }
